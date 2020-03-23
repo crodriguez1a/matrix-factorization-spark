71d4519cfd061648023e183aac8edf238212202a
crodriguez1a <crodriguez1a@gmail.com>
2020-03-22 20:39:57 -0400

    Adds session, fixes module path

--- app/core.py
+++ app/core.py
@@ -1,19 +1,28 @@
 from dataclasses import dataclass
+
 from pyspark import SparkConf, SparkContext, rdd
+from pyspark.sql import SparkSession
+from pyspark.mllib.linalg.distributed import IndexedRow, DenseMatrix
 
-from app.methods.pca import gramian_matrix
+from methods.pca import gramian_matrix
 
-@dataclass
-class SC:
-    app_name: str
-    local: bool = False
 
-    @property
-    def context(self) -> SparkContext:
-        conf: SparkConf = SparkConf().setAppName(self.app_name)
-        return SparkContext.getOrCreate(conf)
-
+def init_session(app_name: str = "mxfs") -> SparkSession:
+    spark: SparkSession = SparkSession \
+                            .builder \
+                            .appName(app_name) \
+                            .getOrCreate()
+    spark.sparkContext.getConf().getAll()
+    return spark
 
 if __name__ == "__main__":
-    app_sc: SC = SC('mxfs').context
-    # TODO
+    spark: SparkSession = init_session()
+    sc: SparkContext = spark.sparkContext
+
+    # TEMP
+    rows: rdd.RDD = sc.parallelize([
+        IndexedRow(0, [1,2,3]),
+        IndexedRow(1, [4,5,6])
+    ])
+    gm: DenseMatrix = gramian_matrix(rows)
+    print(gm.values)
--- test/methods/test_pca.py
+++ test/methods/test_pca.py
@@ -1,9 +1,8 @@
 from app.methods.pca import gramian_matrix
-from app.core import SC
 from pyspark import rdd, SparkContext
 from pyspark.mllib.linalg.distributed import IndexedRow, DenseMatrix
 
-def test_gramian_matrix(spark_context):
+def test_gramian_matrix(spark_context: SparkContext):
     rows: rdd.RDD = spark_context.parallelize([
         IndexedRow(0, [1, 2, 3]),
         IndexedRow(1, [4, 5, 6])
